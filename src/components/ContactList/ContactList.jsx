39f99a3ca5038be62a07c047d68d87773afbf599
Dok6857 <Dok6857@gmail.com>
2024-03-25 15:39:37 +0200

    fix bugs

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,16 +2,10 @@
 import css from './ContactList.module.css';
 import { Contact } from '../Contact/Contact';
 import { useSelector } from 'react-redux';
-import { selectContacts } from '../../redux/contactsSlice';
-import { selectNameFilter } from '../../redux/filtersSlice';
+import { selectVisibleContacts } from '../../redux/contactsSlice';
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const filteredContacts = useSelector(selectVisibleContacts);
 
   return (
     <ul className={css.contactList}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,12 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
 
 const rootReducer = combineReducers({
-  contacts: persistReducer(persistConfig, contactsReducer),
+  contacts: contactsReducer,
   filters: filtersReducer,
 });
 
