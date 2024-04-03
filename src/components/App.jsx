6deb55435ade040051fcb5f7c8787f7c5ef71332
Dok6857 <Dok6857@gmail.com>
2024-04-03 11:26:38 +0300

    made some work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,62 @@
-// App.jsx
-import { useEffect } from 'react';
-import './App.css';
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { SearchBox } from './SearchBox/SearchBox';
-import { fetchContacts } from '../redux/contacts/operations';
-import { useDispatch } from 'react-redux';
+import { lazy, Suspense, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { refreshUser } from "../redux/auth/operations";
+import { selectIsRefreshing } from "../redux/auth/selectors";
+import { RestrictedRoute } from "./RestrictedRoute";
+import { PrivateRoute } from "./PrivateRoute";
+// import css from "./App.module.css";
 
-function App() {
+const HomePage = lazy(() => import("../pages/HomePage"));
+const RegisterPage = lazy(() => import("../pages/RegisterPage"));
+const LoginPage = lazy(() => import("../pages/LoginPage"));
+const ContactsPage = lazy(() => import("../pages/ContactsPage"));
+
+export default function App() {
+  const isRefreshing = useSelector(selectIsRefreshing);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
   return (
-        <div className="container">
-          <h1>Phonebook</h1>
-          <ContactForm />
-          <SearchBox />
-          <ContactList />
-        </div>
+    <div>
+      {isRefreshing ? (
+        <b>Refreshing user, please wait...</b>
+      ) : (
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  component={<RegisterPage />}
+                  redirectTo="/contacts"
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute
+                  component={<LoginPage />}
+                  redirectTo="/contacts"
+                />
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute component={<ContactsPage />} redirectTo="/login" />
+              }
+            />
+          </Routes>
+        </Suspense>
+      )}
+      <Toaster />
+    </div>
   );
 }
-
-export default App;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,33 +1,13 @@
-import { useDispatch } from 'react-redux';
-import { logIn } from '../../redux/auth/operations';
-import css from './LoginForm.module.css';
+import { Helmet } from 'react-helmet';
+import { LoginForm } from '../components/LoginForm/LoginForm';
 
-export const LoginForm = () => {
-  const dispatch = useDispatch();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
-  };
-
+export default function Login() {
   return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label className={css.label}>
-        Password
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <div>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <LoginForm />
+    </div>
   );
-};
+}
